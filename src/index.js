d42b8254e8a0f8423fb975e9b25d18e06288375c
igorfernandes12b <igor.fernandes.jogos@gmail.com>
2025-10-18 22:08:56 -0300

    segundo commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import express from "express"
+import express, { response } from "express"
 import cors from "cors"
 import {persons} from "./persons.js"
 const port = 3333
 const app = express()
 
 app.use(cors())
+app.use(express.json())
 app.get("/", (request, response) => {
     response.json(persons)
+})
+
+app.post("/cadastrar", (request, responde) =>{
+    const { user } = request.body
+
+    console.log( user )
+
+    response.status(201).json({message: "Usuario cadastrado com sucesso!"})
 })
 
 app.listen(port, () =>{
